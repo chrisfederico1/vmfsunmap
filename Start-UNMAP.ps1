707f0a35cc46d9e7907eaa34c21edd58ee07f25d
chrisfederico1 <34996684+chrisfederico1@users.noreply.github.com>
2020-01-07 10:23:34 -0600

    First Commit

---
+++ Start-UNMAP.ps1
@@ -0,0 +1,49 @@
+function Start-UNMAP{
+
+<#
+	.SYNOPSIS
+    Process SCSI UNMAP on VMware Datastore
+    
+	.DESCRIPTION
+    This Function will process SCSI UNMAP on VMware Datastore via ESXCLI -V2. Remember to dot source.
+	.Example
+    Start-UNMAP -VMHost MyHost -DataStore myDataStore 
+	.Example
+    Start-UNMAP -VMHost MyHost -Datastore myDatastore -Verbose -WhatIf
+	.Notes
+	NAME: Start-UNMAP.ps1
+    AUTHOR: Chris Federico  
+	LASTEDIT: 01/03/2020
+	VERSION: 1.0
+	KEYWORDS: VMware, vSphere, ESXi, SCSI, VAAI, UNMAP
+   
+ #>
+    
+# Parameters
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string]$VMHost,
+
+        [Parameter(Mandatory=$true)]
+        [string]$Datastore
+       
+    )
+
+Begin{
+    # Add start-transact here 
+
+}
+
+Process {
+
+
+}
+
+End {
+
+
+}
+
+}
