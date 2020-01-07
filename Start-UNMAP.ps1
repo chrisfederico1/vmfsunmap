6447e0ff806a22122a2cd158668adc399a1b06fb
chrisfederico1 <34996684+chrisfederico1@users.noreply.github.com>
2020-01-07 12:25:38 -0600

    Added code .Still not finished.

--- Start-UNMAP.ps1
+++ Start-UNMAP.ps1
@@ -5,7 +5,8 @@ function Start-UNMAP{
     Process SCSI UNMAP on VMware Datastore
     
 	.DESCRIPTION
-    This Function will process SCSI UNMAP on VMware Datastore via ESXCLI -V2. Remember to dot source.
+    This Function will process SCSI UNMAP on VMware Datastore via ESXCLI -V2. Remember to dot source. Note USE host that has access
+    Datastore.
 	.Example
     Start-UNMAP -VMHost MyHost -DataStore myDataStore 
 	.Example
@@ -13,7 +14,7 @@ function Start-UNMAP{
 	.Notes
 	NAME: Start-UNMAP.ps1
     AUTHOR: Chris Federico  
-	LASTEDIT: 01/03/2020
+	LASTEDIT: 01/07/2020
 	VERSION: 1.0
 	KEYWORDS: VMware, vSphere, ESXi, SCSI, VAAI, UNMAP
    
@@ -33,21 +34,56 @@ function Start-UNMAP{
 
 Begin{
     # Clear Screen
+    Clear-host
+
     # Add start-transact here
- Start-Transaction -path .\vmfsunmaplogging.txt
-write-host "You selected host " $VMHost " with datastore " $Datastore -BackgroundColor Red
+    Start-Transaction -path .\vmfsunmaplogging.txt
+
+    # Give user information about which server and datastore they choose
+    write-host "You selected host " $VMHost " with datastore " $Datastore -BackgroundColor Red
+
+    # Ask user if they would like to continue
+    $reply = Read-host "Would you like to Proceed ?[y/n]"
+    if ($reply -match "[nN]")
+    {
+	exit 
+    }
+
+# Verification of Host and Datastore
+write-host "INFO: Verifying Host"
+
+try {
+
+    $EsxiHost = get-vmhost $VMHost
+}
+
+catch{
+
+    Write-Output "An Error Occured: " "" $error[0] ""
+}
+
+write-host "INFO: Verifiying Datastore"
 
 
+try {
 
+    $DataStoreName = get-datastore $Datastore
+}
 }
 
 Process {
+
+
+
 
 
 }
 
 End {
 
+
+    # Stop Logging
+    Stop-Transcript
 
 }
 
